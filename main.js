ea9f0d00616b4b652f65f763922127a184c4712b
stevenkore <42966699+stevenkore@users.noreply.github.com>
2018-12-18 13:58:30 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const guessField = document.querySelector('.guessField');
 const guessSubmit = document.querySelector('.guessSubmit');
 
 const guesses = document.querySelector('.guesses');
-const lastResults = document.querySelector('.lastResults');
+const lastResult = document.querySelector('.lastResult');
 const lowOrHigh = document.querySelector('.lowOrHigh');
 
 let randomNumber = Math.floor(Math.random() * 100) + 1;
@@ -19,18 +19,39 @@ const checkGuess = () => {
     guesses.textContent += `${userGuess} `;
     
     if (userGuess === randomNumber) {
-        // siia kood, kui kasutaja arvab õigesti
-    } else if (userCount === 10) {
+        //kui kasutaja arvab õigesti
+        lastResult.textContent = 'Palju õnne sa võitsid';
+        lastResult.style.backgroundColor = 'green';
+        document.getElementById("myBtn").disabled = true;
+        document.getElementById("guessField").disabled = true;
+        lowOrHigh.textContent = '';
+    } else if (guessCount === 10) {
         // kui kasutaja paneb 10 korda mööda
+        lastResult.textContent = 'Mäng läbi';
+        document.getElementById("myBtn").disabled = true;
+        document.getElementById("guessField").disabled = true;
+        
     } else {
+        lastResult.textContent = 'Vale vastus!';
+        lastResult.style.backgroundColor = 'red';
+        
+        const lowOrHighText = 'Viimane pakkumine oli ';
+        
         if (userGuess < randomNumber) {
             // liiga madal
+            lowOrHigh.textContent = lowOrHighText + 'liiga madal';
         } else if (userGuess > randomNumber) {
             // liiga kõrge
+            lowOrHigh.textContent = lowOrHighText + 'liiga kõrge';
         };
+}
+
     };
-    
+   
     guessCount++;
+    guessField.value = '';
+    guessField.focus();
+    
 }; 
 
 guessSubmit.addEventListener('click', checkGuess);
